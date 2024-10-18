87e67c4245566b598a19bcd098785907b8fc2b7d
Cody Wakeford <codypwakeford@gmail.com>
2024-10-18 20:05:01 +0100

    minor changes

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,38 +1,14 @@
 #!/bin/bash
 
 echo ""
-echo -e "\e[32m[Veloris] Installing dependencies and spinning up a dev server... \e[0m"
-
-cd /app/dashboard
+echo -e "\e[32m[Nova] Installing dependencies and spinning up a dev server... \e[0m"
 
 echo ""
 echo ""
-echo -e "\e[32m[Veloris] Pulling latest veloris-dashboard from git. \e[0m"
-git pull
-
-echo ""
-echo ""
-echo -e "\e[32m[Veloris] Installing veloris-dashboard dependencies. \e[0m"
+echo -e "\e[32m[Nova] Installing dependencies. \e[0m"
 pnpm install
 
 echo ""
 echo ""
-echo -e "\e[32m[Veloris] Building veloris-dashboard for .nuxt cache. \e[0m"
-nuxt build
-
-cd /app/site
-
-echo ""
-echo ""
-echo -e "\e[32m[Veloris] Pulling latest veloris-site-template from git. \e[0m"
-git pull
-
-echo ""
-echo ""
-echo -e "\e[32m[Veloris] Installing veloris-site-template dependencies. \e[0m"
-pnpm install
-
-echo ""
-echo ""
-echo -e "\e[32m[Veloris] Starting development server... \e[0m"
+echo -e "\e[32m[Nova] Starting development server... \e[0m"
 nuxt dev --no-fork
